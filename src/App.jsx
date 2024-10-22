0684b68adbabb29b2b2d321dfa7c324b056297f8
Mario <mariosmilcic@gmail.com>
2024-10-22 12:44:47 +0200

    added navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./features/Home/home";
+import About from "./features/About/About";
+import Contact from "./features/Contact/contact";
+import Navigation from "./features/Navigation/Navigation";
+import LogIn from "./features/LogIn/logIn";
+import CreatePost from "./features/CreatePost/CreatePost";
+
 const App = () => {
   return (
     <>
-      <h1>Blog</h1>
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<LogIn />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
