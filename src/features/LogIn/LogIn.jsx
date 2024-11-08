9e49f4c0d83e44003358b05c440bb48fd895219b
Mario <mariosmilcic@gmail.com>
2024-11-08 12:32:15 +0100

    fixed naming

--- src/features/LogIn/LogIn.jsx
+++ src/features/LogIn/LogIn.jsx
@@ -6,6 +6,7 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "../../validation/loginSchema";
 import { loginInputs } from "./helpers/loginInputs";
 import Button from "../../components/Button/Button";
+import Login from "./Login";
 
 const Login = () => {
   const [isNewUser, setIsNewUser] = useState(false);
